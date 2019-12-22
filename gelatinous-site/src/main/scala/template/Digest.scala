7fb2fd96a66675c857be8e7cd691900b83d19231
Kyle F. Davies <kylefdavies@gmail.com>
2019-12-22 18:17:12 +0700

    Move gelatinous.site into main project
    Could move it out later

--- gelatinous-site/src/main/scala/template/Digest.scala
+++
@@ -1,6 +0,0 @@
-package gelatinous.site.template
-
-class Digest(post: Post) {
-  import scalatags.Text.all._
-  val myHtml = frag(h2(post.pageTitle), p("TODO"), p("TODO"))
-}
--- gelatinous-site/src/test/scala/Site.scala
+++
@@ -1,7 +0,0 @@
-// package gelatinous.site
-
-// class SiteSuite extends org.scalatest.FunSuite {
-//   test("Site") {
-//     Site.build()
-
-// }
